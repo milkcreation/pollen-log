a4179784e3d22bb5f200f93fd66007624b6166e4
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-02-15 11:42:38 +0000

    log >> 1.0.x-dev

--- log/branches/1.0/src/Logger.php
+++ log/branches/1.0/src/Logger.php
@@ -8,13 +8,35 @@ use Monolog\Handler\HandlerInterface;
 use Monolog\Logger as BaseLogger;
 use Pollen\Support\Concerns\ContainerAwareTrait;
 use Pollen\Support\Concerns\ParamsBagTrait;
-
+use Psr\Log\InvalidArgumentException;
 
 class Logger extends BaseLogger implements LoggerInterface
 {
     use ContainerAwareTrait;
     use LoggerDefaultHandlersTrait;
     use ParamsBagTrait;
+
+    /**
+     * @var int
+     */
+    public const SUCCESS = 225;
+
+    /**
+     * This is a static variable and not a constant to serve as an extension point for custom levels
+     *
+     * @var array<int, string> $levels Logging levels with the levels as key
+     */
+    protected static $levels = [
+        self::DEBUG     => 'DEBUG',
+        self::INFO      => 'INFO',
+        self::SUCCESS   => 'SUCCESS',
+        self::NOTICE    => 'NOTICE',
+        self::WARNING   => 'WARNING',
+        self::ERROR     => 'ERROR',
+        self::CRITICAL  => 'CRITICAL',
+        self::ALERT     => 'ALERT',
+        self::EMERGENCY => 'EMERGENCY',
+    ];
 
     /**
      * @var HandlerInterface[]|array
@@ -34,10 +56,49 @@ class Logger extends BaseLogger implements LoggerInterface
     }
 
     /**
+     * Conversion de niveau de journalisation PSR-3.
+     * {@internal Hack en vue de la validation du niveau success}
+     *
+     * @param string|int $level
+     *
+     * @return int
+     *
+     * @throws InvalidArgumentException If level is not defined
+     */
+    public static function toMonologLevel($level): int
+    {
+        if (is_string($level)) {
+            if (is_numeric($level)) {
+                return (int)$level;
+            }
+
+            $upper = strtr($level, 'abcdefgilmnorstuwy', 'ABCDEFGILMNORSTUWY');
+            if (defined(__CLASS__ . '::' . $upper)) {
+                return constant(__CLASS__ . '::' . $upper);
+            }
+
+            throw new InvalidArgumentException(
+                'Level "' . $level . '" is not defined, use one of: ' . implode(', ', array_keys(static::$levels))
+            );
+        }
+
+        if (!is_int($level)) {
+            throw new InvalidArgumentException(
+                'Level "' . var_export($level, true) . '" is not defined, use one of: ' . implode(
+                    ', ',
+                    array_keys(static::$levels)
+                )
+            );
+        }
+
+        return $level;
+    }
+
+    /**
      * @inheritDoc
      */
     public function success(string $message, array $context = []): void
     {
-        $this->notice($message, $context);
+        $this->addRecord(self::SUCCESS, $message, $context);
     }
 }
